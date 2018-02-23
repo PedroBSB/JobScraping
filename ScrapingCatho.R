6294238d19ad24c3b5528cbb518904d5719ad64f
Pedro <pedro.melo.albuquerque@gmail.com>
2018-02-23 14:47:48 -0300

    Scraping from empregos.com.br

--- ScrapingCatho.R
+++ ScrapingCatho.R
@@ -3,11 +3,34 @@ library(rvest)
 library(purrr)
 library(RSelenium)
 #Define the link with all positions
-link <- read_html("https://www.vagas.com.br/vagas-de-Brasil?")
+link <- read_html("https://www.catho.com.br/vagas/?q=&pais_id=31&estado_id%5B%5D=&regiao_id%5B%5D=&cidade_id%5B%5D=&cidade=")
 
-#City
-city <- link %>% 
-  html_nodes(xpath='//*[@id="pesquisaFiltros"]/div[2]') %>% 
+#Pages
+pages <- link %>% 
+         html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "paginacao", " " ))]') %>% 
+         html_nodes('a') %>% html_attr('href')
+
+#Find the last page
+pos <- regexpr('page=', pages)+5
+last<- max(as.numeric(substr(pages,pos[1],nchar(pages))))
+
+for(i in 1:last){
+  link<-paste0("https://www.catho.com.br/vagas/?pais_id=31&where_search=1&how_search=2&faixa_sal_id_combinar=1&order=score&perfil_id=1&page=",i)
+  
+  pages <- link %>% 
+    html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "viewVagaAction", " " ))]') 
+  
+  
+  %>% 
+    html_nodes('a') %>% html_attr('href')
+  
+  
+  
+}
+
+
+
+%>% 
   html_nodes("ul") %>% 
   html_nodes("li") %>% 
   html_nodes('a') %>% html_attr('href')
---
+++ ScrapingEmpregos.R
@@ -0,0 +1,87 @@
+#Call the libraries
+library(rvest)
+library(purrr)
+library(RSelenium)
+library(tidyverse)
+library(stringr)
+#Define the link with all positions
+link <- read_html("https://www.empregos.com.br/vagas")
+
+#Pages
+pages <- link %>% 
+  html_nodes(xpath='//*[@id="ctl00_ContentBody_pPaiResultadoTopo"]/strong[2]') %>% 
+  html_text()
+
+#Find the last page
+posIni <- regexpr('de ', pages)+3
+subsPage<- substr(pages,posIni,nchar(pages))
+posEnd <- regexpr('\r',subsPage)-1
+last<- as.numeric(gsub("[.]","",substr(subsPage,1,posEnd)))
+
+
+pb <- txtProgressBar(min = 0, max = last)
+for(i in 1:last){
+  #Define the link with all positions
+  link <- read_html(paste0("https://www.empregos.com.br/vagas/p",i))
+  
+  #Links
+  jobLinks <- link %>% 
+    html_nodes(xpath='//*[@id="ctl00_ContentBody_divPaiMioloBusca"]/ul')%>% 
+    html_nodes('a') %>% html_attr('href')
+  #Filter the links
+  links<-grepl("vagas", jobLinks)
+  jobLinks<-jobLinks[links]
+  write.table(jobLinks, file="Data\\EmpregosLinks.txt", append=TRUE, col.names = FALSE, row.names=FALSE, quote = FALSE) 
+  setTxtProgressBar(pb, i)
+}
+close(pb)
+
+#Read the links
+links<-read.table("Data\\EmpregosLinks.txt")
+last<-nrow(links)
+pb <- txtProgressBar(min = 0, max = last)
+for(i in 1:last){
+  #Define the link with all positions
+  link <- read_html(as.character(links[i,]))
+  
+  #Position
+  position <- link %>% 
+    html_nodes(xpath='//*[@id="ctl00_ContentBody_h1TituloVaga"]')%>% 
+    html_text()
+  
+  #Firm
+  firm <- link %>% 
+    html_nodes(xpath='//*[@id="ctl00_ContentBody_hplEmpresa"]')%>% 
+    html_text()
+  
+  #Information Job
+  information <- link %>% 
+    html_nodes(xpath='//*[@id="ctl00_ContentBody_divDetalheVaga"]/div[2]/div/article/div[4]/div/table[1]')%>% 
+    html_table() 
+  info.df1<-information[[1]]
+  info.df1 <-info.df1 %>% spread(X1,X2) 
+  if(ncol(info.df1)==4){
+    info.df1<-cbind("",info.df1)
+  }
+  colnames(info.df1)<-c("Obs", "Description", "Requirements", "Place", "Qualification")
+  #Information Firm
+  information <- link %>% 
+    html_nodes(xpath='//*[@id="ctl00_ContentBody_divDetalheVaga"]/div[2]/div/article/div[4]/div/table[2]')%>% 
+    html_table()
+  info.df2<-information[[1]]
+  info.df2 <-info.df2 %>% spread(X1,X2) 
+  colnames(info.df2)<-c("DescriptionFirm", "Firm", "Sector")  
+  temp<-cbind(info.df1,info.df2)
+  temp2<-as.data.frame(lapply(temp,function(x)str_trim(gsub("[\r\n]", "", as.character(x)))))
+  
+  if(i==1){
+    write.table(temp2, file="Data\\EmpregosScraping.txt",sep = ";", append=FALSE, col.names = TRUE, row.names=FALSE, quote = FALSE)
+  }
+  else{
+    write.table(temp2, file="Data\\EmpregosScraping.txt",sep = ";", append=TRUE, col.names = FALSE, row.names=FALSE, quote = FALSE)
+  }
+  
+  setTxtProgressBar(pb, i)
+  print(i)
+}
+close(pb)
