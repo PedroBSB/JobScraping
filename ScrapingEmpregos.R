c4cc7da9106ffa8505cbd66f783b1ff32cc6f1b4
Pedro Albuquerque <pedro.melo.albuquerque@gmail.com>
2018-02-26 09:23:18 -0300

    Update script

--- ScrapingEmpregos.R
+++ ScrapingEmpregos.R
@@ -20,7 +20,7 @@ last<- as.numeric(gsub("[.]","",substr(subsPage,1,posEnd)))
 
 
 pb <- txtProgressBar(min = 0, max = last)
-for(i in 1:last){
+for(j in 1:last){
   #Define the link with all positions
   link <- read_html(paste0("https://www.empregos.com.br/vagas/p",i))
   
@@ -30,9 +30,9 @@ for(i in 1:last){
     html_nodes('a') %>% html_attr('href')
   #Filter the links
   links<-grepl("vagas", jobLinks)
-  jobLinks<-jobLinks[links]
+  jobLinks<-data.frame(j,jobLinks[links])
   write.table(jobLinks, file="Data\\EmpregosLinks.txt", append=TRUE, col.names = FALSE, row.names=FALSE, quote = FALSE) 
-  setTxtProgressBar(pb, i)
+  setTxtProgressBar(pb, j)
 }
 close(pb)
 
@@ -43,7 +43,7 @@ last<-nrow(links)
 pb <- txtProgressBar(min = 0, max = last)
 for(i in 1:last){
   #Define the link with all positions
-  link <- read_html(as.character(links[i,]))
+  link <- read_html(as.character(links[i,2]))
   
   #Position
   position <- link %>% 
@@ -64,22 +64,25 @@ for(i in 1:last){
   if(ncol(info.df1)==4){
     info.df1<-cbind("",info.df1)
   }
-  colnames(info.df1)<-c("Obs", "Description", "Requirements", "Place", "Qualification")
+  if(ncol(info.df1)==3){
+    info.df1<-cbind("","",info.df1)
+  }
   #Information Firm
   information <- link %>% 
     html_nodes(xpath='//*[@id="ctl00_ContentBody_divDetalheVaga"]/div[2]/div/article/div[4]/div/table[2]')%>% 
     html_table()
   info.df2<-information[[1]]
   info.df2 <-info.df2 %>% spread(X1,X2) 
-  colnames(info.df2)<-c("DescriptionFirm", "Firm", "Sector")  
   temp<-cbind(info.df1,info.df2)
   temp2<-as.data.frame(lapply(temp,function(x)str_trim(gsub("[\r\n]", "", as.character(x)))))
+  
+  temp2<-data.frame("I"=i,temp2)
   
   if(i==1){
     write.table(temp2, file="Data\\EmpregosScraping.txt",sep = ";", append=FALSE, col.names = TRUE, row.names=FALSE, quote = FALSE)
   }
   else{
-    write.table(temp2, file="Data\\EmpregosScraping.txt",sep = ";", append=TRUE, col.names = FALSE, row.names=FALSE, quote = FALSE)
+    write.table(temp2, file="Data\\EmpregosScraping.txt",sep = ";", append=TRUE, col.names = TRUE, row.names=FALSE, quote = FALSE)
   }
   
   setTxtProgressBar(pb, i)
