f2726dbd8cf104813821d64bd7c3d59b59415185
Pedro <pedro.melo.albuquerque@gmail.com>
2018-03-06 11:59:40 -0300

    New Code

---
+++ ScrapingStackOverflow.R
@@ -0,0 +1,68 @@
+#Call the libraries
+library(rvest)
+library(purrr)
+library(RSelenium)
+library(tidyverse)
+library(stringr)
+#Define the link with all positions
+link <- read_html("https://stackoverflow.com/jobs?sort=i")
+
+#Pages
+pages <- link %>% 
+  html_nodes(xpath='//*[@id="index-hed"]/div[1]/span') %>% 
+  html_text()
+
+#Find the last page
+pages <- gsub('[\r\n\t]', '', pages)
+posEnd <- regexpr(' ', pages)[1]-1
+last<- as.numeric(substr(pages[1],1,posEnd))
+pagesNum <- ceiling(last/25)
+
+pb <- txtProgressBar(min = 0, max = last)
+for(j in 1:pagesNum){
+  #Define the link with all positions
+  link <- read_html(paste0("https://stackoverflow.com/jobs?sort=i&pg=",i))
+  
+  #Links
+  jobLinks <- link %>% 
+    html_nodes(xpath='//*[@id="content"]/div/div[2]/div/div[1]/div[2]/div')%>% 
+    html_nodes('a') %>% html_attr('href')
+  #Filter the links
+  links<-!(grepl("/jobs/developer-", jobLinks) | grepl("/jobs/devops", jobLinks) | grepl("/jobs/docker-", jobLinks) |  grepl("/jobs/designer-", jobLinks) | grepl("/jobs/system-", jobLinks))
+  jobLinks<-data.frame(j,jobLinks[links])
+  write.table(jobLinks, file="Data\\StackOverFlowLinks.txt", append=TRUE, col.names = FALSE, row.names=FALSE, quote = FALSE) 
+  setTxtProgressBar(pb, j)
+}
+close(pb)
+
+#Read the links
+links<-read.table("Data\\StackOverFlowLinks.txt")
+links<-unique(links)
+last<-nrow(links)
+pb <- txtProgressBar(min = 0, max = last)
+for(i in 1:last){
+  #Define the link with all positions
+  link <- read_html(paste0("https://stackoverflow.com/",as.character(links[i,2])))
+  
+  #Position
+  position <- link %>% 
+    html_nodes(xpath='//*[@id="job-detail"]/div[1]/div/div[2]/div[1]/h1/a')%>% 
+    html_text()
+  
+  #Firm
+  firm <- link %>% 
+    html_nodes(xpath='//*[@id="job-detail"]/div[1]/div/div[2]/div[2]/div[1]/a')%>% 
+    html_text()
+  
+  #Information Job
+  technologies <- link %>% 
+    html_nodes(xpath='//*[@id="overview-items"]/section[2]/div') %>% 
+    html_nodes('a') %>% html_attr('href')
+  description <- link %>%   html_node(xpath='//*[@id="overview-items"]/section[3]') %>%     html_text()
+  description <- gsub('[\r\n\t]', '', description)
+  
+  temp2<-data.frame("Position"=position,"Firm"=firm,"Description"=description,"Link"=paste0("https://stackoverflow.com/",as.character(links[i,2])))
+  write.table(temp2, file="Data\\EmpregosStackOverflow.txt",sep = ";", append=FALSE, col.names = TRUE, row.names=FALSE, quote = FALSE)
+  setTxtProgressBar(pb, i)
+}
+close(pb)
--- ScrapingVagas.R
+++ ScrapingVagas.R
@@ -5,58 +5,92 @@ library(RSelenium)
 #Define the link with all positions
 link <- read_html("https://www.vagas.com.br/vagas-de-Brasil?")
 
-#City
-city <- link %>% 
-  html_nodes(xpath='//*[@id="pesquisaFiltros"]/div[2]') %>% 
-  html_nodes("ul") %>% 
-  html_nodes("li") %>% 
-  html_nodes('a') %>% html_attr('href')
+
+
+#Call the libraries
+library(rvest)
+library(purrr)
+library(RSelenium)
+library(tidyverse)
+library(stringr)
+#Define the link with all positions
+link <- read_html("https://www.empregos.com.br/vagas")
+
+#Pages
+pages <- link %>% 
+  html_nodes(xpath='//*[@id="ctl00_ContentBody_pPaiResultadoTopo"]/strong[2]') %>% 
+  html_text()
+
+#Find the last page
+posIni <- regexpr('de ', pages)+3
+subsPage<- substr(pages,posIni,nchar(pages))
+posEnd <- regexpr('\r',subsPage)-1
+last<- as.numeric(gsub("[.]","",substr(subsPage,1,posEnd)))
+
+
+pb <- txtProgressBar(min = 0, max = last)
+for(i in 1:last){
+  #Define the link with all positions
+  link <- read_html(paste0("https://www.empregos.com.br/vagas/p",i))
   
+  #Links
+  jobLinks <- link %>% 
+    html_nodes(xpath='//*[@id="ctl00_ContentBody_divPaiMioloBusca"]/ul')%>% 
+    html_nodes('a') %>% html_attr('href')
+  #Filter the links
+  links<-grepl("vagas", jobLinks)
+  jobLinks<-jobLinks[links]
+  write.table(jobLinks, file="Data\\EmpregosLinks.txt", append=TRUE, col.names = FALSE, row.names=FALSE, quote = FALSE) 
+  setTxtProgressBar(pb, i)
+}
+close(pb)
 
-#City link
-link.cty<-paste0("https://www.vagas.com.br",city[1])
-
-#Define the download folder
-eCaps <- list(
-  chromeOptions = 
-    list(prefs = list(
-      "profile.default_content_settings.popups" = 0L,
-      "download.prompt_for_download" = FALSE,
-      "download.default_directory" = getwd()
-    )
-    )
-)
-
-#Check drivers
-rD <- rsDriver(extraCapabilities = eCaps)
-
-#Client RSelenium
-remDr <- rD$client
-remDr <- remoteDriver(port = 4567L, browserName = "chrome")
-
-#Open Browser
-remDr$open()
-
-#Maximize window
-remDr$maxWindowSize()
-
-#Go to web page
-remDr$navigate(link.cty)
-
-#Encontra o objeto da caixa de texto
-webElem <- remDr$findElement('xpath', '//*[@id="maisDeOntem"]')
-
-#Clica no botao
-webElem$clickElement()
-
-#Wait the download
-Sys.sleep(5)
-
-
-
-
-
-#Fecha as conexoes
-remDr$close()
-remDr$closeServer()
-
+#Read the links
+links<-read.table("Data\\EmpregosLinks.txt")
+last<-nrow(links)
+pb <- txtProgressBar(min = 0, max = last)
+for(i in 1:last){
+  #Define the link with all positions
+  link <- read_html(as.character(links[i,]))
+  
+  #Position
+  position <- link %>% 
+    html_nodes(xpath='//*[@id="ctl00_ContentBody_h1TituloVaga"]')%>% 
+    html_text()
+  
+  #Firm
+  firm <- link %>% 
+    html_nodes(xpath='//*[@id="ctl00_ContentBody_hplEmpresa"]')%>% 
+    html_text()
+  
+  #Information Job
+  information <- link %>% 
+    html_nodes(xpath='//*[@id="ctl00_ContentBody_divDetalheVaga"]/div[2]/div/article/div[4]/div/table[1]')%>% 
+    html_table() 
+  info.df1<-information[[1]]
+  info.df1 <-info.df1 %>% spread(X1,X2) 
+  if(ncol(info.df1)==4){
+    info.df1<-cbind("",info.df1)
+  }
+  colnames(info.df1)<-c("Obs", "Description", "Requirements", "Place", "Qualification")
+  #Information Firm
+  information <- link %>% 
+    html_nodes(xpath='//*[@id="ctl00_ContentBody_divDetalheVaga"]/div[2]/div/article/div[4]/div/table[2]')%>% 
+    html_table()
+  info.df2<-information[[1]]
+  info.df2 <-info.df2 %>% spread(X1,X2) 
+  colnames(info.df2)<-c("DescriptionFirm", "Firm", "Sector")  
+  temp<-cbind(info.df1,info.df2)
+  temp2<-as.data.frame(lapply(temp,function(x)str_trim(gsub("[\r\n]", "", as.character(x)))))
+  
+  if(i==1){
+    write.table(temp2, file="Data\\EmpregosScraping.txt",sep = ";", append=FALSE, col.names = TRUE, row.names=FALSE, quote = FALSE)
+  }
+  else{
+    write.table(temp2, file="Data\\EmpregosScraping.txt",sep = ";", append=TRUE, col.names = FALSE, row.names=FALSE, quote = FALSE)
+  }
+  
+  setTxtProgressBar(pb, i)
+  print(i)
+}
+close(pb)
